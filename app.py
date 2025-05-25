83b7fd75f2d8cb2e626c2b3611fe7e8449ca7d9a
Vishwas Tiwari <vishwas.2022ug3026@iiitranchi.ac.in>
2025-05-26 00:27:38 +0530

    Initial commit: Flask app with regression model

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
-from flask import Flask, request, jsonify, render_template
+import json
 import pickle
+
+from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
+import pandas as pd
 
-app = Flask(__name__)
-
-# Load the trained model
-with open('model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
-
+app=Flask(__name__)
+## Load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
 def predict():
-    features = [float(x) for x in request.form.values()]
-    final_features = np.array(features).reshape(1, -1)
-    prediction = model.predict(final_features)
-    return render_template('index.html', prediction_text='Predicted House Price: ${:.2f}'.format(prediction[0]))
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+   
+     
